d9f69515b64b37dfc803d5f103cee53c5cdacfee
pkuwalter <pkuwalter@qq.com>
2018-08-28 17:34:06 +0800

    local

--- src/interColor.cu
+++ src/interColor.cu
@@ -122,6 +122,11 @@ void merge_colors_on_cpu(
 		{
 			if (copy_num[i]>1)
 			{
+
+				for (int j = 0; j < gpu_num; ++j)
+				{
+					color_gpu[i] = h_add_color[j][i];  
+				}
 				/**************************
 				int num_undone = std::count(uncolored, uncolored + vertex_num, 1);
 				
@@ -129,6 +134,8 @@ void merge_colors_on_cpu(
 				if(percentage < (float)num_undone/vertex_num)
 					break;
 				**********************/
+
+				/**************************
 				new_color=0.0f;
 				for (int j = 0; j < gpu_num; ++j)
 				{
@@ -138,6 +145,7 @@ void merge_colors_on_cpu(
 				if(fabs(new_color- color_gpu[i]>PAGERANK_THRESHOLD))
 					//flag=1;
 				color_gpu[i]=new_color;
+				**********************/
 			}
 		}
 	}
