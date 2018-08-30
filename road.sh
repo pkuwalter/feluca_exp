19a7587337ffdb8b014ef1cccddaa40743a1ed91
pkuwalter <pkuwalter@qq.com>
2018-08-30 17:53:04 +0800

    830

--- road.sh
+++ road.sh
@@ -6,4 +6,4 @@ make
 
 datapath=/vpublic01/frog/zhengzhigao/feluca4big/example
 
-./vpublic01/frog/zhengzhigao/feluca_exp/exp ${datapath}/roadnet/roadnet.vertices ${datapath}/roadnet/roadnet.edges 735292 3523247 539587 1761624 4
+./exp ${datapath}/roadnet/roadnet.vertices ${datapath}/roadnet/roadnet.edges 735292 3523247 539587 1761624 4
